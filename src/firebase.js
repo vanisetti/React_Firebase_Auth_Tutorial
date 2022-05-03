776d1da2ba802fde09318fd2182532775fcc8221
vanisetti <vijaya.anisetti@gmail.com>
2022-05-02 20:00:26 -0400

    test 1

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: <YOURAPIKEY>,
-  authDomain: <YOURAUTHDOAMIN>,
-  projectId: <YOURPROJECTID>,
-  storageBucket: <YOURSTORAGEBUCKET>,
-  messagingSenderId: <YOURMESSAGINGSENDERID>,
-  appId: <YOURAPPID>,
+  apiKey: "AIzaSyAnK77USgJJDK25tRWCT-XmQJR3G_YbfZc",
+  authDomain: "test-4a086.firebaseapp.com",
+  projectId: "test-4a086",
+  storageBucket: "test-4a086.appspot.com",
+  messagingSenderId: "624139890038",
+  appId: "1:624139890038:web:d7a424731d9b9727a25e57",
+  measurementId: "G-0G77HHK9XQ"
 };
 
 // Initialize Firebase
